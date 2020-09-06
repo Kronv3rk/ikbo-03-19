ec29418557a547801f7cfe6beed259aca8fa144c
okone211 <70575808+okone211@users.noreply.github.com>
2020-09-06 22:29:42 +0300

    Delete Two.java

--- Two.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.lab1;
-
-public class Two {
-    public static void main (String[]args){
-        for(int i=0;i<args.length;i++){
-            System.out.println(args[i]);
-        }
-    }
-}
