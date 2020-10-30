83d19a6831cfdb3d9df16d17d541de3138263864
okone211 <70575808+okone211@users.noreply.github.com>
2020-10-30 12:49:23 +0300

    Add files via upload

---
+++ Practice12/src/ru/Ex4.java
@@ -0,0 +1,12 @@
+package ru;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        PhoneNumber p = new PhoneNumber("+44518323410");
+        System.out.println(p);
+        p = new PhoneNumber("89464562387");
+        System.out.println(p);
+    }
+}
+
+
